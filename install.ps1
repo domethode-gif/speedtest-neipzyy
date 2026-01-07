8ef5502880eb3fac17280c806fec64581a419e87
domethode-gif <domethode@gmail.com>
2026-01-07 07:36:31 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,4 +1,4 @@
 $bin="$env:USERPROFILE\speedtest-neipzyy.exe"
-$url="https://github.com/USERNAME/speedtest-neipzyy/releases/latest/download/speedtest-neipzyy.exe"
+$url="https://github.com/domethode-gif/speedtest-neipzyy/releases/latest/download/speedtest-neipzyy.exe"
 Invoke-WebRequest $url -OutFile $bin
 Write-Host "✅ Installed! Restart terminal & run: speedtest-neipzyy"
